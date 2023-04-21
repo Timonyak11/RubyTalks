966958654dc601acf224aba94ced09ac4b485433
Kirt Brian Reguyal <118667385+Timonyak11@users.noreply.github.com>
2023-04-21 20:49:51 +0800

    UI fixed

--- app/assets/javascripts/talkings/conversation.js
+++ app/assets/javascripts/talkings/conversation.js
@@ -1,10 +1,13 @@
-let currentChallenge = parseInt(localStorage.currentChallenge,10) || 0; 
-let challenge;
-const challenges = gon.challenges;
+let currentChallenge = parseInt(localStorage.currentChallenge,10) || 0; // Gets the currentChallenge data from the localStorage if null then it's set to 0
+let challenge; 
+const challenges = gon.challenges; // Gets all the Challenges passed from the controller to the view file
 
-const user_progress = [];
-
-const progress = JSON.parse(localStorage.getItem("progress")) || user_progress;
+let progress = localStorage.getItem("progress");
+if (progress && progress !== "") {
+	progress = JSON.parse(progress);
+} else {
+	progress = [];
+}
 
 $(document).ready(function() {
 	loadchallenge();
@@ -15,29 +18,94 @@ $(document).ready(function() {
 	editor.setTheme("ace/theme/vibrant_ink");
 	editor.session.setMode("ace/mode/ruby");
 
-	$('div#challenges ol li').on('click', function(){
-		currentChallenge = parseInt($(this).attr('id'));
+	editor.container.addEventListener("keypress", function(event) {
+		if (event.shiftKey && event.keyCode === 13) {
+			event.preventDefault();
 
-		editor.setValue('');
-		resetresponse();
-		loadchallenge();
-		showchallengedesc();
+			editor.insert("\n");
+		} 
+		else if(event.keyCode === 13) {
+			event.preventDefault();
+
+			$('form').submit();
+		}
+
 	});
 
 	// Realtime update of the response or the output of the users answer
 	$('form').on('submit', function(e){
 		e.preventDefault();
 		let answer = editor.getValue();
-		
-		$.post($(this).attr('action'), $(this).serialize() + "&answer=" + answer, function(res) {
-			verifyOutput(res);
-			$('#you').html(res.replaceAll("\n", "<br>"))
+		let modified_answer = encodeURIComponent(answer);
+
+		$.post($(this).attr('action'), $(this).serialize() + "&answer=" + modified_answer + "&challenge=" + currentChallenge, function(res) {
+			let correct = verifyOutput(res);
+			$('#you').removeAttr('class');
+			
+			if(correct){	
+				$('#level').append("<img id='rubybadge' src = '" + rubybadge + "' alt = 'alt'>");
+				$('#you').addClass("correct");
+				$('#rubygem').attr('src', rubycorrect);
+				$('#ruby').html("You got it right!");
+				$('#you').html("Lezgooooo!");
+
+				setTimeout(function() {
+					editor.setValue('');
+					resetresponse();
+					updatechallengeslist();
+					loadchallenge();
+				}, 2500);
+			} else{
+				if(res === 'Error'){
+					$('#you').addClass("wrong");
+					$('#ruby').html("Not even close.");
+					$('#rubygem').attr('src', rubywrong);
+					$('#you').html("I'm sorry, let me try again.");
+
+					setTimeout(function() {
+						$('#ruby').html(challenge.rubysays);
+						$('#rubygem').attr('src', rubyasks);
+						$('#you').html(". . .");
+						$('#you').removeClass("wrong");
+					}, 2500);
+				} else{
+					$('#you').addClass("wrong");
+					$('#ruby').html("Come on you can do it!");
+					$('#rubygem').attr('src', rubyyoucandoit);
+					$('#you').html("Okay, Let me try again.");
+
+					setTimeout(function() {
+						$('#ruby').html(challenge.rubysays);
+						$('#rubygem').attr('src', rubyasks);
+						$('#you').html(". . .");
+					}, 2500);
+				}
+			}
 		});
 	});
 
 	// Shows all the challenges by clicking the dropdown
 	$('.dropdown-menu').on('click', function(){
 		showchallenges();
+	});
+
+	$('div#challenges ol li').on('click', function(){
+		let listID = $(this).attr('id');
+		currentChallenge = parseInt(listID.match(/\d+/)[0]);
+		
+		editor.setValue('');
+		resetresponse();
+		loadchallenge();
+		showchallengedesc();
+	});
+
+	$('button#reset').on('click', function(){
+		editor.setValue('');
+
+		resetGame();
+		loadchallenge();
+		showchallengedesc();
+		updatechallengeslist();	
 	});
 
 	// Closes the view to all challenges then proceeds to show the description of a challenge
@@ -71,7 +139,30 @@ $(document).ready(function() {
 });
 
 function verifyOutput(output){
-	console.log(output === challenge.expected_output);
+	let result = false;
+	for(let expect_index = 0; expect_index < challenge.expected_output.length; expect_index++){
+		if(output === challenge.expected_output[expect_index]){
+			let existing = false;
+			for(let i = 0; i < progress.length; i++){
+				if(progress[i] === currentChallenge){
+					existing = true;
+				}
+			}
+	
+			if(existing == false){
+				progress.push(currentChallenge);
+			}
+			
+			localStorage.setItem("progress", JSON.stringify(progress));
+			
+			currentChallenge++;
+			if(currentChallenge >= challenges.length) {
+				currentChallenge = challenges.length - 1;
+			}
+			result = true;
+		}
+	}
+	return result;
 }
 
 function showchallenges(){
@@ -84,7 +175,6 @@ function showchallengedesc(){
 	$('div#challenge_desc').delay(400).slideDown();
 }
 
-
 function loadchallengeslist(){
 	let chapter;
 	let list = "<ol>";
@@ -92,13 +182,18 @@ function loadchallengeslist(){
 		if(i > 0){
 			chapter = challenges[i-1].title;
 			if(chapter != challenges[i].title){
-				list += "<p>" + challenges[i].title + "</p>"; 
+				list += "<p class='chapter'>" + challenges[i].title + "</p>"; 
 			}
 		} else{
-			list += "<p id='chapter'>" + challenges[i].title + "</p>"; 
+			list += "<p class='chapter'>" + challenges[i].title + "</p>"; 
 		}
 		
-		list += "<li id='" + i + "'>" + challenges[i].method + "</li>";
+		list += "<li id='c" + i + "'>" + challenges[i].topic;
+		let iscomplete = ischallengecomplete(i);
+		if(iscomplete == true){
+			list += "<img class='rubybadge' src = '" + rubybadge + "' alt = 'alt'>"
+		}
+		list += "</li>";
 	}
 	list += "</ol>";
 	$('div#challenges').html(list);
@@ -121,18 +216,44 @@ function loadchallenge(){
 }
 
 function loadconvofield(){
-    $('#dothis').html(challenge.dothis);
+	let rubyprovides = challenge.rubyprovide || '';
+
+	let editor = ace.edit("editor");	
+	editor.session.setValue(rubyprovides);  
+	
+	$('#dothis').html(challenge.dothis);
     $('#ruby').html(challenge.rubysays);
+    $('#you').html('. . .');
+    $('#rubygem').attr('src', rubyasks);
 }
 
 function loaddescription(){
+	let method = challenge.method || '';
+	let methoddesc = challenge.methoddesc || '';
+
+	if(method == ''){
+		$('#method').hide();
+		$('#methoddesc').hide();
+	} else{
+		$('#method').show();
+		$('#methoddesc').show();
+	}
+
+	$('#topic').html(challenge.topic);
+	$('#topicdesc').html(challenge.topicdesc);
 	$('#title').html(challenge.title);
     $('#subtitle').html(challenge.subtitle);
-    $('#method').html(challenge.method);
-    $('#help').html(challenge.help);
-    $('#level').html("Challenge " + (currentChallenge+1) + " of " + (challenges.length));
+    $('#method').html(method);
+    $('#methoddesc').html(methoddesc);
 
-	let examples;
+	$('#level').html('Challenge ' + (currentChallenge+1) + " of " + (challenges.length));
+	
+	let iscomplete = ischallengecomplete(currentChallenge);
+	if(iscomplete){
+		$('#level').append("<img id='rubybadge' src = '" + rubybadge + "' alt = 'alt'>");
+	}
+
+	let examples = '';
     for(let i = 0; i < challenge.examples.length; i++){
         examples += challenge.examples[i];
     }
@@ -141,4 +262,35 @@ function loaddescription(){
 
 function resetresponse(){
 	$('#you').html('');	
+}
+
+function ischallengecomplete(thechallenge){
+	let complete = false;
+	for(let i = 0; i < progress.length; i++){
+		if(thechallenge === progress[i]){
+			complete = true;
+		}
+	}
+
+	return complete;
+}
+
+function updatechallengeslist(){
+	let allchallenges = document.querySelectorAll('ol li');
+	for(let challengeID = 0; challengeID < allchallenges.length; challengeID++){
+		let result = ischallengecomplete(challengeID);
+		if(result == true){
+			$('div#challenges_list ol li#c' + (challengeID)).append("<img class='rubybadge' src = '" + rubybadge + "' alt = 'rubyimage'>");
+		} else{
+			$('div#challenges_list ol li#c' + (challengeID) + ' img').remove();
+		}
+	}
+}
+
+function resetGame(){
+	currentChallenge = 0;
+	progress = [];
+
+	localStorage.setItem("currentChallenge",currentChallenge);
+	localStorage.setItem("progress", JSON.stringify(progress));
 }
--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,58 +1,207 @@
+require 'cgi'
 class ChallengesController < ApplicationController
-    @@output = ''
-    @double_quote = '"'
+    @@challenges = [
+        # {
+        #     title: "Printing",
+        #     subtitle: "Print your response to Ruby using puts",
+        #     topic: "puts",
+        #     topicdesc: "<strong>puts</strong> is a method that stands for <b>'put string'</b>, It is used to output a string to the console followed by a newline character. You can put it inside <b>double quotes(\")</b> or <b>single quotes(')</b> either way the compiler will still consider it as a string.",
+        #     examples: ["<p><strong>puts \"Hello World!\"</strong> prints Hello World!</p>", "<p><strong>puts 'Coding Dojo!'</strong>  prints Coding Dojo!"],
+        #     dothis: "Respond by printing 'Hello Ruby!' using puts",
+        #     expected_output: ["Hello Ruby!\n"],
+        #     rubysays: "Ruby says Hi!"
+        # },
+        # {
+        #     title: "Printing",
+        #     subtitle: "Print your response to Ruby using print",
+        #     topic: "print",
+        #     topicdesc: "the <strong>print</strong> method works just like the puts method, except it doesn't add a newline character to the end of the output. You can put it inside <b>double quotes(\")</b> or <b>single quotes(')</b> either way the compiler will still consider it as a string.",
+        #     examples: ["<p><strong>print \"Hello World!\"</strong> prints Hello World!</p>", "<p><strong>print 'Coding Dojo!'</strong> prints Coding Dojo!</p>"],
+        #     dothis: "Respond by printing 'Hello Ruby!' using print",
+        #     expected_output: ["Hello Ruby!"],
+        #     rubysays: "Ruby says Hi!"
+        # },
+        {
+            title: "Data Types",
+            subtitle: "Create a String",
+            # method: ".class",
+            # methoddesc: "In Ruby, <strong>.class</strong> is a method that can be called on an object to determine data type. Refer to the examples below to get the correct response to Ruby.",
+            examples: ["<p><strong>\"Hello Ruby!\"</strong></p>", "<p><strong>'This is Ruby!'</strong></p>"],
+            dothis: "Ruby asks you to create a String",
+            expected_output: ["String", "String\n"],
+            rubysays: "Can you create a String object?",
+            # rubyprovide: "\"Sample Object\"",
+            topic: "String",
+            topicdesc: "<p>A <b>string</b> is a collection of letters, numbers, and symbols that can be created using quotes, either double quotes <b>\"...\"</b> or single quotes <b>'...'</b>.</p>"
+        },
+        {
+            title: "Data Types",
+            subtitle: "Create an Integer",
+            # method: ".class",
+            # methoddesc: "In Ruby, <strong>.class</strong> is a method that can be called on an object to determine data type. Refer to the examples below to get the correct response to Ruby.",
+            examples: ["<p><strong>200</strong></p>", "<p><strong>-250</strong></p>"],
+            dothis: "Ruby asks you to create an Integer",
+            expected_output: ["Integer", "Integer\n"],
+            rubysays: "Can you create an Integer object?",
+            # rubyprovide: "69",
+            topic: "Integer",
+            topicdesc: "<p>An <b>integer</b> is a data type that represents whole numbers, both positive and negative from <b>-2,147,483,648</b> to <b>2,147,483,647</b>. It is a type of numerical data that can be used in calculations and comparisons.</p>"
+        },
+        {
+            title: "Data Types",
+            subtitle: "Create a Float",
+            # method: ".class",
+            # methoddesc: "In Ruby, <strong>.class</strong> is a method that can be called on an object to determine data type. Refer to the examples below to get the correct response to Ruby.",
+            examples: ["<p><strong>69.420</strong></p>", "<p><strong>666.444</strong></p>"],
+            dothis: "Ruby asks you to create a Float",
+            expected_output: ["Float", "Float\n"],
+            rubysays: "Can you create a Float object?",
+            # rubyprovide: "69.420\n",
+            topic: "Float",
+            topicdesc: "<p>A <b>float</b> is a data type that represents numbers with decimal points. It is a type of numerical data that can be used in calculations and comparisons where a higher level of precision is required.</p>"
+        },
+        {
+            title: "Data Types",
+            subtitle: "Create an Array",
+            # method: ".class",
+            # methoddesc: "In Ruby, <strong>.class</strong> is a method that can be called on an object to determine data type. Refer to the examples below to get the correct response to Ruby.",
+            examples: ["<p><strong>[4,5,6,7,8]</strong></p>", "<p><strong>[2,4.5,3,'kirt','Reguyal']</strong></p>"],
+            dothis: "Ruby asks you to create an Array",
+            expected_output: ["Array", "Array\n"],
+            rubysays: "Can you create an Array object?",
+            # rubyprovide: "[1,3,4]",
+            topic: "Array",
+            topicdesc: "<p>An <b>array</b> in Ruby is a collection of elements of the same or different data types. Arrays are often used to store and manipulate data retrieved from a database or obtained from user input.</p>"
+        },
+        {
+            title: "Data Types",
+            subtitle: "Create a Hash",
+            # method: ".class",
+            # methoddesc: "In Ruby, <strong>.class</strong> is a method that can be called on an object to determine data type. Refer to the examples below to get the correct response to Ruby.",
+            examples: ["<p><strong>{\"Alice\" => 25, \"Bob\" => 30, \"Charlie\" => 35}</strong></p>", "<p><strong>{ name: \"John\", age: 30, occupation: \"Developer\" }</strong></p>"],
+            dothis: "Ruby asks you to create a Hash",
+            expected_output: ["Hash", "Hash\n"],
+            rubysays: "Can you create a Hash object?",
+            # rubyprovide: "[1,3,4]",
+            topic: "Hash",
+            topicdesc: "<p>a <b>hash</b> is a data structure that allows you to store a collection of key-value pairs. A key-value pair is made up of a key (also called a \"symbol\") and a corresponding value.</p>"
+        },
+        {
+            title: "Variables",
+            subtitle: "Create a variable and store any valid data to it.",
+            method: "Variables",
+            methoddesc: "<b>Variables</b> are used to <i>store</i> values or objects. They act as placeholders for values that can change or be updated during the execution of the program.",
+            examples: ["<p><strong>variableName = \"This is a sting\"</strong></p>", "<p><strong>anyName = 214</strong></p>"],
+            dothis: "Ruby asks you to create a variable <br>and assign any data type in it.",
+            expected_output: ["isavariable"],
+            rubysays: "Can you create a variable and store any object in it?",
+            # rubyprovide: "[1,3,4]",
+            topic: "Assignment",
+            topicdesc: "<p>To assign a value to a variable, you use an equal sign <b>(=)</b> and write the variable name on the left side, followed by the value you want to assign to it on the right side.</p>"
+        },
+        {
+            title: "Variables",
+            subtitle: "Do interpolation by using the provided variable.",
+            method: "Variables",
+            methoddesc: "<b>Variables</b> are used to <i>store</i> values or objects. They act as placeholders for values that can change or be updated during the execution of the program",
+            examples: ["<p><strong>puts \"Hello \#{name}\"</strong></p>", "<p><strong>print \"Sum of 2 numbers \#{sum}\"</strong></p>"],
+            dothis: "Ruby asks you to use Interpolation",
+            expected_output: ["Hello World!", "Hello World!\n"],
+            rubysays: "Use ",
+            rubyprovide: "sample = \" World!\"",
+            topic: "Interpolation",
+            topicdesc: "<p>Interpolation is a technique used in programming to <b>combine</b> variables and strings into a single, cohesive output.</p>"
+        }
+    ]
 
     def index
-        gon.challenges = [
-            {
-                title: "Printing",
-                subtitle: "Print your response to Ruby using puts",
-                method: "puts",
-                help: "<strong>puts</strong> is a method that stands for <b>'put string'</b>, It is used to output a string to the console followed by a newline character.",
-                examples: ["<p><strong>puts 'Hello World!'</strong> prints Hello World!</p>", "<p><strong>puts 'Coding Dojo!'</strong>  prints Coding Dojo!"],
-                dothis: "Respond by printing 'Hello Ruby!'",
-                expected_output: "Hello Ruby!\n",
-                rubysays: "Ruby says Hi!"
-            },
-            {
-                title: "Printing",
-                subtitle: "Print your response to Ruby using print",
-                method: "print",
-                help: "the <strong>print</strong> method works just like the puts method, except it doesn't add a newline character to the end of the output. The print method is used to output a string to the console without adding a newline character.",
-                examples: ["<p><strong>print 'Hello World!'</strong> prints Hello World!</p>", "<p><strong>print 'Coding Dojo!'</strong> prints Coding Dojo!</p>"],
-                dothis: "Respond by printing 'Hello Ruby!'",
-                expected_output: "Hello Ruby!",
-                rubysays: "Ruby says Hi!"
-            },
-            {
-                title: "Data Types",
-                subtitle: "Check the data type of the given data.",
-                method: ".class",
-                help: "the <strong>print</strong> method works just like the puts method, except it doesn't add a newline character to the end of the output. The print method is used to output a string to the console without adding a newline character.",
-                examples: ["<p><strong>print 'Hello World!'</strong> prints Hello World!</p>", "<p><strong>print 'Coding Dojo!'</strong> prints Coding Dojo!</p>"],
-                dothis: "What do you think is the data type of this data given by Ruby?",
-                expected_output: "Hello Ruby!",
-                rubysays: "Ruby says Hi!"
-            }
-        ]
+        gon.challenges = @@challenges
     end
 
-    def run
+    def run 
+        # begin
+        #     eval(params[:answer])
+        #     output = capture_stdout { 
+        #         eval("puts #{params[:answer]}.class")
+        #     }
+        # rescue SyntaxError => e
+        #     output = 'Syntax Error'
+        # rescue => e
+        #     output = 'Error'
+        # end
+        output = ''
+        challenge = @@challenges[params[:challenge].to_i]
+        if(challenge[:title] == "Data Types")
+            output = verifyDataType(params[:answer])
+        elsif(challenge[:title] == "Variables")
+            if(challenge[:topic] == "Interpolation")
+                output = verifyInterpo(params[:answer])
+            else
+                output = verifyVariables(params[:answer])
+            end
+        end
+
+        puts output
+        
+        render plain: output
+    end
+
+    private 
+    def verifyDataType(data)
         begin
-            eval(params[:answer])
+            if data == ''
+                data = 'error'
+            end
+
+            eval(data)
             output = capture_stdout { 
-                eval(params[:answer])
+                eval("var = #{data}\nputs var.class")
             }
         rescue SyntaxError => e
             output = 'Syntax Error'
         rescue => e
             output = 'Error'
         end
-        
-        render plain: output
+
+        output
     end
 
-    private 
+    def verifyVariables(data)
+        data_arr = data.split('=')
+        var_name = data_arr[0]
+        var_value = data_arr[1]
+        
+        # begin
+        #     if data == ''
+        #         data = 'error'
+        #     end
+
+        #     # if var_value = ''
+        #     #     data = 'error'
+        #     # end
+
+        #     eval("#{data}\nputs #{var_name}")
+        #     output = 'isavariable'
+        # rescue SyntaxError => e
+        #     output = 'Syntax Error'
+        # rescue => e
+        #     output = 'Error'
+        # end
+        variable_declaration_pattern = /^[a-zA-Z_]+[a-zA-Z0-9_]*\s*=\s*.*$/
+
+        if data =~ variable_declaration_pattern
+            if var_value === ' '
+                output = 'is not valid'
+            else
+                output = 'isavariable'
+            end
+        else
+            output = 'isnotavariable'
+        end
+
+        output
+    end
+
     def capture_stdout
         buffer = StringIO.new
         original_stdout = $stdout
